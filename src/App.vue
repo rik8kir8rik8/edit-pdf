e3ae093d9b3732450a7095aa87addfc79acf5498
rik8kir8rik8 <rik_kir_888@yahoo.co.jp>
2019-08-30 17:11:34 +0900

    api

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <form id="form" method="post" accept-charset="utf-8" return false>
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" id="name" class="form-control" name="name"><br>
+    </div>
+    <div class="form-group">
+      <label for="text">Text:</label>
+      <textarea id="text" class="form-control" name="text"></textarea>
+    </div>
+    </form>
+    <input type="button" id="sub" class="btn btn-dark" v-on:click="postClick" value="OK">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue'
+import axios from 'axios';
+Vue.prototype.$axios = axios;
 
 export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  methods: {
+    postClick:function(){
+      const nametxt = document.getElementById('name').value;
+      const txt = document.getElementById('text').value;
+      const textData = { name: nametxt ,text: txt};
+   
+    axios.post('http://localhost:8080/post', textData)
+    .then(res => {
+      alert("「" + res.name + "」登録完了");
+    })
+    .catch(error=> {
+        alert("「" + error.name + "」登録失敗");
+    });
+    }
+  },
 }
 </script>
 
